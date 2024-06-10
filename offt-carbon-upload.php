f7c3a9045eaf10ee97f593e47b8d88a2284a9925
ivankrivenko <keyv@narod.ru>
2024-06-10 15:19:14 +1000

    Плагин загрузки и отображения файлов на статических страницах

---
+++ offt-carbon-upload.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Offt Carbon Upload
+Plugin URI:  http://example.com
+Description: Плагин загрузки и отображения файлов на статических страницах
+Version:     1.0
+Author:      Your Name
+*/
