284aa1ca2ee45276a8f597c0a6ab705682951b50
ivankrivenko <keyv@narod.ru>
2024-06-20 17:59:37 +1000

    Готовим файлы к выгрузке в репозиторий

--- cf-file-display.php
+++ cf-file-display.php
@@ -1,37 +1,46 @@
 <?php
 /*
-Plugin Name: Offt Carbon Upload
-Plugin URI:  http://example.com
-Description: Плагин загрузки и отображения файлов на статических страницах
+Plugin Name: CF File Display
+Plugin URI:  https://ivankrivenko.ru
+Description: Uploading and displaying a list of files in posts and static pages based on Carbon Fields
 Version:     1.0
-Author:      Your Name
+Author:      Ivan Krivenko
+Text Domain: cf-file-display
+Domain Path: /languages
 */
 if ( ! function_exists( 'is_plugin_active' ) ) {
     require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 }
 
-function my_custom_plugin_check_carbon_fields() {
+function cffd__load_textdomain() {
+    load_plugin_textdomain('cf-file-display', false, dirname(plugin_basename(__FILE__)) . '/languages');
+}
+add_action('plugins_loaded', 'cffd__load_textdomain');
+
+
+function cffd_check_carbon_fields() {
     if ( ! is_plugin_active( 'carbon-fields/carbon-fields-plugin.php' ) ) {
         // Carbon Fields не активен, добавляем уведомление в админку
-        add_action('admin_notices', 'my_custom_plugin_carbon_fields_admin_notice');
+        add_action('admin_notices', 'cffd_carbon_fields_admin_notice');
 
         // Прекращаем выполнение основных функций плагина
-        add_action('admin_init', 'my_custom_plugin_deactivate');
+        add_action('admin_init', 'cffd_deactivate');
     } else {
 
     }
 }
-add_action('admin_init', 'my_custom_plugin_check_carbon_fields');
+add_action('admin_init', 'cffd_check_carbon_fields');
 
-function my_custom_plugin_carbon_fields_admin_notice() {
+function cffd_carbon_fields_admin_notice() {
     ?>
-    <div class="notice notice-error">
-        <p><?php _e('Your Custom Plugin requires the Carbon Fields plugin to be active. Please activate Carbon Fields to use this plugin.', 'your-textdomain'); ?></p>
-    </div>
-    <?php
+<div class="notice notice-error">
+    <p><?php _e('CF File Display Plugin requires the Carbon Fields plugin to be active. Please activate Carbon Fields to use this plugin.', 'cf-file-display'); ?>
+    </p>
+</div>
+<?php
 }
 
-function my_custom_plugin_deactivate() {
+function cffd_deactivate() {
     // Здесь можно деактивировать основные функции вашего плагина
     // Например, отменить регистрацию кастомных полей, настроек и т.д.
 }
@@ -42,40 +51,47 @@ use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
 
-add_action('carbon_fields_register_fields', 'crb_attach_theme_options');
-function crb_attach_theme_options() {
-    Container::make('post_meta', __('Additional Files', 'your-textdomain'))
+add_action('carbon_fields_register_fields', 'crb_attach_file');
+function crb_attach_file() {
+    Container::make('post_meta', __('Attached files', 'cf-file-display'))
         ->where('post_type', '=', 'page') // Ограничить поля только для страниц
         
         ->add_fields(array(
             Field::make('complex', 'crb_files', __('Files'))
 
                 // Множественное поле
-                ->add_fields( 'folder', 'Папка', array(
-                    Field::make( 'text', 'folder', 'Название папки'  ),
-                    Field::make( 'rich_text', 'folder_description', 'Описание папки' ),
-                    Field::make( 'complex', 'files', 'Вложенные файлы' )
-                        ->add_fields( 'file', 'Файл', array(
-                            Field::make( 'text', 'file_description', 'Название файла'  ),
-                            Field::make( 'file', 'file', 'Файл' )
+                ->add_fields( 'folder', __('Folder', 'cf-file-display'), array(
+                    Field::make( 'text', 'folder', __('Folder name', 'cf-file-display')  ),
+                    Field::make( 'rich_text', 'folder_description', __('Folder description', 'cf-file-display') ),
+                    Field::make( 'complex', 'files', __('Attached files', 'cf-file-display') )
+                        ->add_fields( 'file', __('File', 'cf-file-display'), array(
+                            Field::make( 'text', 'file_description', __('File description', 'cf-file-display') ),
+                            Field::make( 'file', 'file', __('File', 'cf-file-display') )
                         ))
                 ))
                 
 
-                ->add_fields( 'file', 'Файл', array(
-                    Field::make( 'text', 'file_description', 'Название файла'  ),
-                    Field::make( 'file', 'file', 'Файл' )
+                ->add_fields( 'file', __('File', 'cf-file-display'), array(
+                    Field::make( 'text', 'file_description', __('File description', 'cf-file-display')  ),
+                    Field::make( 'file', 'file', __('File', 'cf-file-display') )
                 ))
         ));
 }
-
+/*
 add_action('after_setup_theme', 'crb_load');
 function crb_load() {
     \Carbon_Fields\Carbon_Fields::boot();
 }
-
+*/
 function formatBytes($bytes, $precision = 2) {
-    $units = array('Б', 'Кб', 'Мб', 'Гб', 'Тб');
+
+    $byte = __('B', 'cf-file-display');
+    $kb = __('KB', 'cf-file-display');
+    $mb = __('MB', 'cf-file-display');
+    $gb = __('GB', 'cf-file-display');
+    $tb = __('TB', 'cf-file-display');
+
+    $units = array($byte, $kb, $mb, $gb, $tb);
 
     $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
@@ -100,51 +116,54 @@ function display_file_item($file) {
         } else {
             $file_name = get_the_title($file_id); // Получение имени файла
         }
-
-        
+       
         $file_extension = pathinfo($file_url, PATHINFO_EXTENSION); // Получение расширения файла
         ?>
-        <a href="<?php echo esc_url($file_url); ?>" class="attached-files-list-item">
-            <?php echo esc_html($file_name); ?>
-            <span class="attached-file-extension"><?php echo formatBytes($filesize); ?></span>
-        </a>
-        <?php
+<a href="<?php echo esc_url($file_url); ?>" target="_blank" class="attached-files-list-item">
+    <?php echo esc_html($file_name); ?>
+    <?php if ($filesize != 0) { ?>
+    <span class="attached-file-extension"><?php echo formatBytes($filesize); ?></span>
+    <?php } ?>
+</a>
+<?php
     }
 }
 
 
 
 // Hook to display the files
-add_filter('the_content', 'my_custom_display_files');
-function my_custom_display_files($content) {
+add_filter('the_content', 'cffd_display_files');
+function cffd_display_files($content) {
     if (is_page()) { // Проверка, что это страница
 
 
         $files = carbon_get_the_post_meta('crb_files');
 
+        /*
         echo '<pre>';
         print_r($files);
         echo '</pre>';
-
+        */
         if ($files) {
             ?>
 
-            <h4>Прикрепленные файлы</h4>
-            <div class="attached-files-list">
-            <?php
+<h4><?php echo __('Attached files', 'cf-file-display') ?></h4>
+<div class="attached-files-list">
+    <?php
             $folder = false;
             foreach ($files as $file) {
                 if ($file['_type'] == 'file') {
-                                        // Если показываем файлы после папки
-                                        if ($folder == true) {
-                                            $folder = false;
-                                            ?>
-                                            </div>
-                                            <div class="attached-files-list">
-                                            <?php
-                                        }
-                    display_file_item($file);
 
+                    // Если показываем файлы после папки
+                    if ($folder == true) {
+                        $folder = false;
+                        ?>
+</div>
+<div class="attached-files-list">
+    <?php
+                    }
+
+                    display_file_item($file);
 
                 }
 
@@ -154,18 +173,18 @@ function my_custom_display_files($content) {
                     $folder = true;
 
                     ?>
-                        </div>
-                        <div class="attached-files-list">
-                        <div class="attached-files-list-header"><?php echo $file['folder']; ?></div>
-                        <div class="attached-files-list-content">
-                            <?php if (!empty($file['folder_description'])) { ?>
-                            
-                            <p><?php echo $file['folder_description']; ?></p>
+</div>
+<div class="attached-files-list">
+    <div class="attached-files-list-header"><?php echo $file['folder']; ?></div>
+    <div class="attached-files-list-content">
+        <?php if (!empty($file['folder_description'])) { ?>
 
-                            <?php } ?>
-                            
-                            <div class="attached-files-list">
-                                <?php 
+        <p><?php echo $file['folder_description']; ?></p>
+
+        <?php } ?>
+
+        <div class="attached-files-list">
+            <?php 
                                     if (isset( $file['files'])) {
                                         foreach ($file['files'] as $folders_file) {
                                             display_file_item($folders_file);
@@ -177,19 +196,17 @@ function my_custom_display_files($content) {
                                         }
                                     }
                                 ?>
-                            </div>
-                        </div>
-                    <?php
-
+        </div>
+    </div>
+    <?php
                     
                 }
                 
-                
             }
             ?>
-              </div>  
-              
-            <?php
+</div>
+
+<?php
         }
 
     }
@@ -227,4 +244,3 @@ function enqueue_attached_files_script() {
 
 // Хук для подключения скриптов и стилей
 add_action('wp_enqueue_scripts', 'enqueue_attached_files_script');
-
---
+++ readme.txt
@@ -0,0 +1,53 @@
+=== CF File Display ===
+Contributors: ivankrivenko
+Tags: file, files, upload, display, carbon fields
+Requires at least: 4.7
+Tested up to: 6.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Uploading and displaying a list of files in posts and static pages based on Carbon Fields.
+
+== Description ==
+
+CF File Display allows you to upload and display a list of files in posts and static pages using the Carbon Fields library. This plugin is perfect for managing and showcasing documents, images, and other files on your WordPress site.
+
+= Features =
+* Upload files to posts and static pages.
+* Display files in a structured list.
+* Supports various file types.
+* Easy to use and configure.
+
+== Installation ==
+
+1. Upload the plugin files to the `/wp-content/plugins/cf-file-display` directory, or install the plugin through the WordPress plugins screen directly.
+2. Activate the plugin through the 'Plugins' screen in WordPress.
+3. Configure the plugin settings under the 'Settings' menu in the WordPress admin panel.
+
+== Frequently Asked Questions ==
+
+= How do I upload files? =
+You can upload files directly from the post or page edit screen where the Carbon Fields are displayed.
+
+= Can I display files on custom post types? =
+Yes, you can configure the plugin to display files on custom post types by modifying the plugin settings.
+
+== Screenshots ==
+
+1. **File Upload** - Upload files from the post or page edit screen.
+2. **File Display** - Display files in a structured list on the front end.
+
+== Changelog ==
+
+= 1.0 =
+* Initial release of CF File Display.
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial release. No upgrade steps required.
+
+== License & Copyright ==
+
+This plugin is licensed under the GPLv2 or later. For more information, see http://www.gnu.org/licenses/gpl-2.0.html.
